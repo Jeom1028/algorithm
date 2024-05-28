2b0419ba502abd4be13873d241c0734d52586ff6
Jeom1028 <168401241+Jeom1028@users.noreply.github.com>
2024-05-28 23:17:15 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ Swift/프로그래머스/0/120831. 짝수의 합/짝수의 합.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    
+    var answer = 0
+    
+        for i in 1...n where i%2 == 0 {
+                answer += i
+            }
+    
+    return answer
+}
