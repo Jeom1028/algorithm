d577d191c8c31d48e98ad7d8399fd3e59d95e972
Jeom1028 <168401241+Jeom1028@users.noreply.github.com>
2024-05-29 09:03:46 +0900

    [level 0] Title: 배열의 평균값, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ Swift/프로그래머스/0/120817. 배열의 평균값/배열의 평균값.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ arr:[Int]) -> Double {
+  var sum = 0
+    
+  for i in 0..<arr.count {
+    sum += arr[i]
+  }
+    
+  return Double(sum) / Double(arr.count)
+}
